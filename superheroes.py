44371588f1b43dca96ed9cbf6d1e4c707ef4ab13
Youssef_Sawiris <youssef.sawiris@students.makeschool.com>
2019-09-24 10:42:38 -0700

    [add] Hero Class intial

--- superheroes.py
+++ superheroes.py
@@ -4,8 +4,8 @@ import random
 class Ability:
     def __init__(self, name, max_damage):
         ''' Create Instance Variables:
-            name: String 
-            max_damage: Integer 
+             name: String 
+             max_damage: Integer 
         '''
         #TODO: Instatiate the variables listed in the docstring with then 
         #values passed in 
@@ -20,6 +20,40 @@ class Ability:
         attack_strength = random.randint(0, self.max_damage)
         return attack_strength
 
+class Armor:
+    def __init__(self, name, max_block):
+        '''Instantiate instance properties.
+            name: String
+            max_block: Integer
+        '''
+        #TODO: Create intance variables for the values passed in
+        self.name = name 
+        self.max_block = max_block
+    
+    def block(self):
+        ''' Return a random value between 0 and the initialized max_block strength. '''
+        block_strength = random.randint(0, self.max_block)
+        return block_strength
+
+class Hero:
+    def __init__(self, name, starting_health):
+        '''Instance properties:
+          abilities: List
+          armors: List
+          name: String
+          starting_health: Integer
+          current_health: Integer
+        '''
+        # TODO: Initialize instance variables values as instance variables
+        # (Some of these values are passed in above,
+        # others will need to be set at a starting value)
+        # abilities and armors are lists that will contain objects that we can use
+        self.abilities = []
+        self.armors = []
+        self.name = name
+        self.starting_health = starting_health
+        self.current_health = 100
+
 
 if __name__ == "__main__":
     #If you run this file from the terminal 
@@ -27,3 +61,13 @@ if __name__ == "__main__":
     ability = Ability("Debugging Ability", 20)
     print(ability.name)
     print(ability.attack())
+
+    armor = Armor("Debugging Armor", 20)
+    print(armor.name)
+    print(armor.block())
+
+    my_hero = Hero("Grace Hopper", 200)
+    print(my_hero.name)
+    print(my_hero.starting_health)
+    print(my_hero.current_health)
+    
