fb2560942abc97c1c4f610bdadf429d5de952e51
Youssef_Sawiris <youssef.sawiris@students.makeschool.com>
2019-09-24 18:33:38 -0700

    [add] Hero attack

--- superheroes.py
+++ superheroes.py
@@ -58,6 +58,19 @@ class Hero:
         ''' Add ability to abilities list '''
         #TODO: Add ability object to abilities:List
         self.abilities.append(ability)
+    
+    def attack(self):
+        '''Calculate the total damage from all ability attacks.
+          return: total:Int
+        '''
+        # TODO: This method should run Ability.attack() on every ability
+        # in self.abilities and returns the total as an integer.
+        total_damage = 0 
+
+        for ability in self.abilities:
+               total_atack =  Ability.attack(ability)
+               total_damage += total_atack
+        return total_damage
 
 
 
@@ -65,6 +78,7 @@ if __name__ == "__main__":
     #If you run this file from the terminal 
     #this block is excuted 
     ability = Ability("Debugging Ability", 20)
+    another_ability = Ability("Smarty Pants", 90)
     print(ability.name)
     print(ability.attack())
 
@@ -73,10 +87,13 @@ if __name__ == "__main__":
     print(armor.block())
 
     my_hero = Hero("Grace Hopper", 200)
+
     print(my_hero.name)
     print(my_hero.starting_health)
     print(my_hero.current_health)
-    hero.add_ability(ability)
-    print(hero.abilities)
+    my_hero.add_ability(ability)
+    my_hero.add_ability(another_ability)
+    print(my_hero.abilities)
+    print(my_hero.attack())
     
     
